9f2289057c8692b276561c265f145ac4c54e8130
JOHN RIVER LUNGAY <202210383@NCBA_TY.EDU.PH>
2025-07-19 15:51:08 +0800

    initial commit

---
+++ mvc2025/Models/College.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvc2025.Models
+{
+    public class College
+    {
+
+        public class student {
+
+            public string student_no { get; set; }
+
+            public string name { get; set; }
+
+            public string course { get; set; }
+
+            public string address { get; set; }
+
+
+
+        }
+
+    }
+}
